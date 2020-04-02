337daad475de0b4501f06ca0c38932503802fa56
Dimiter 'malkia' Stanev <malkia@gmail.com>
2020-04-02 00:01:39 -0700

    blah

--- core.cpp
+++ core.cpp
@@ -1,4 +1,10 @@
-extern "C" int core_version()
+#ifdef _MSC_VER
+#define EXPORT_C extern "C" __declspec(dllexport)
+#else
+#define EXPORT_C extern "C"
+#endif
+
+EXPORT_C int core_version()
 {
     return 123;
 }
--- runner.cmd
+++ runner.cmd
@@ -1,1 +1,6 @@
+echo BALLLS!
+dir
+dir __main__
+pushd %~dp0
+dir *.dart /s/b
 dart %*
